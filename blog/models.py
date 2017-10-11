96dde4bd59971802c575f9e7cfc1c8ac4095415c
Luis Fuentes <luisenriquef0609@gmail.com>
2017-10-11 15:48:22 -0600

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+
 class Post(models.Model):
     author = models.ForeignKey(User)
     title = models.CharField(max_length=200)
